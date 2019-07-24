0bec45f8db8e16dce5f29f0078c3ec1bd2ee48c7
Brookee <oliver.brooke.a@gmail.com>
2019-07-24 01:11:02 -0400

    serverJS/start api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,11 @@
-var friendsInfo = require("../data/friends");
+// Link to Friends.js data 
+var friendsInfo = require("../data/friends.js");
+
+// Friend enteries 
+function apiRoute(app) {
+app.get("api/friends", function(req, res){
+    res.join(friendsInfo);
+});
+}
+// Export to server.js 
+module.exports = apiRoute;
--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 // NPM installed 7/18
+// Required NPM packages
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
+
+//Express
+var app = express();
+
+// PORT 
+var PORT = process.env.PORT || 8080;
+
+// access to CSS file
+app.use(express.static(__dirname + "public/css"));
+
+// Body parsing middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+// Application routes
+require(path.join(__dirname, "./app/routing/apiRoutes.js"));
+require(path.join(__dirname, "./app/routing/htmlRoutes.js"));
+
+
+//Listening on PORT
+app.listen(PORT, function(){
+    console.log("Listening on PORT: "+ PORT);
+});
