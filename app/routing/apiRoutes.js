16c988e790f5af588ca019d1c56ab8e04d8c163a
Brookee <oliver.brooke.a@gmail.com>
2019-07-24 23:31:51 -0400

    revise results logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
     res.json(friendsInfo);
   });
   // New Celeb Friend
-  var newCelebScore = req.body.score;
+  var newCeleb = req.body;
   var scoreArr = [];
-//   var celebCnt = 0;
+  //   var celebCnt = 0;
   var celebMatch = 0;
 
   // For loop to run through celeb list
@@ -18,24 +18,25 @@ module.exports = function(app) {
     var diffScore = 0;
 
     // Compare Celeb friends and go through scores
-    for(var k = 0; k<newCelebScore.length; k++){
-        diffScore += (Math.abs(parseInt(friendsInfo[i].score[k]) - parseInt(newCelebScore[k])));
+    for (var k = 0; k < newCeleb.length; k++) {
+      diffScore += Math.abs(
+        parseInt(friendsInfo[i].score[k]) - parseInt(newCelebScore[k])
+      );
     }
     scoreArr.push(diffScore);
   }
 
-  // Find match 
-  for(var i=0; scoreArr.length; i++){
-    if(scoreArr[i] <= scoreArr[celebMatch]){
-        celebMatch = i;
+  // Find match
+  for (var i = 0; scoreArr.length; i++) {
+    if (scoreArr[i] <= scoreArr[celebMatch]) {
+      celebMatch = i;
     }
   }
-  // return match data 
+  // return match data
   var fMatch = friendsInfo[celebMatch];
   res.json(fMatch);
 
-  // Adds to friendsInfo array 
+  // Adds to friendsInfo array
 
   friendsInfo.push(req.body);
 };
-
